14cf272bb1287e03449c5dd6dd5b16717937340d
Raúl Varandela <61058725+raulvarandela@users.noreply.github.com>
2020-07-06 15:03:35 +0200

    Cambiado los CC

--- ix58gj/View/Footer.php
+++ ix58gj/View/Footer.php
@@ -4,7 +4,7 @@
 	<?php echo $strings['Hoy es'];?> <?php echo date("d-M-Y", mktime()); ?> <!--muestra la fecha-->
 	<br/>
 	<br/>
-	<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src='../View/img/cc.png' /></a><br />Este obra está bajo una <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">licencia de Creative Commons Reconocimiento 4.0 Internacional</a>.
+	<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src='../View/img/cc.png' /></a><br />Este obra está bajo una <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">licencia de Creative Commons Reconocimiento-NoComercial-SinObraDerivada 4.0 Internacional</a>.
 </footer>
 </body>
 </html>
